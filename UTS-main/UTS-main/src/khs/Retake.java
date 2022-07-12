21444d4d0a245356f81e14fc275677558985f971
DerickCc <derickchainatra@gmail.com>
2022-07-13 02:24:54 +0700

    Update Retake.java

--- UTS-main/UTS-main/src/khs/Retake.java
+++ UTS-main/UTS-main/src/khs/Retake.java
@@ -1,7 +1,6 @@
 package khs;
 
 public class Retake extends KhsDetail {
-    private Tagihan tagihan;
     private Term retakeTerm;
     
     public Retake(String kodeKHSDetail, String kodeKHS, Matakuliah mataKuliah, int nilai, Term retakeTerm) {
@@ -16,14 +15,4 @@ public class Retake extends KhsDetail {
     public void tampilkanMataKuliahRetake() {
         System.out.printf("%-20s %-25s %-5d\n", getDetailMatakuliah().getKodeMataKuliah(), getDetailMatakuliah().getNamaMataKuliah(), getDetailMatakuliah().getSks());
     }   
-
-    public float getJumlahTagihan(){
-        setTagihan();
-        return tagihan.jumlahTagihan();
-    }
-
-    public void setTagihan() {
-        this.tagihan = new Tagihan(getDetailMatakuliah().getSks()*500000);
-    }
-    
 }
